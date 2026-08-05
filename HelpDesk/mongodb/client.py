c41bb603a1ec3312ad2c890b320d118ee8e564fd
yuminvoke <yuminkym5@gmail.com>
2026-08-05 13:06:38 +0900

    fix: add authentication

--- HelpDesk/mongodb/client.py
+++ HelpDesk/mongodb/client.py
@@ -1,3 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from pymongo import AsyncMongoClient
 
-client = AsyncMongoClient("mongodb://localhost:27017/?directConnection=true")
+load_dotenv()
+
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+
+client = AsyncMongoClient(
+    f"mongodb://{DB_USER}:{DB_PASSWORD}@localhost:27017/"
+    "?directConnection=true&authSource=admin"
+)
